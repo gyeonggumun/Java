d7ddda2e65fa1a2d44b771344b2938e0f08a5c2e
gyeonggumun <lion989072@gmail.com>
2025-03-13 21:47:16 +0900

    Add files via upload

---
+++ Example01.java
@@ -0,0 +1,11 @@
+package exam;
+
+public class Example01 {
+	public static void main(String[] args) {
+		// 실행단축키 : Ctrl + F11
+		// 파일 저장 단축키 : Ctrl + s
+		// 이전으로 되돌리기 : Ctrl + z
+		// 자동완성 단축키 : Ctrl + 스페이스
+		System.out.println("Hello World");
+	}
+}
---
+++ Quiz1.java
@@ -0,0 +1,19 @@
+package exam;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 출력문을 이용해
+		 * 자신의 이름, 나이, 성별, 거주지를
+		 * 출력하는 프로그램을 작성하세요 !!
+		 */
+						
+		System.out.println("이름 : 문경구");
+		System.out.println("나이 : 25");
+		System.out.println("성별 : 남");
+		System.out.println("거주지 : 부산시 연제구");
+		
+	}
+
+}
