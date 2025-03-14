40786a8c34396ed0aa12c66604d13f76effc7cde
gyeonggumun <lion989072@gmail.com>
2025-03-14 14:30:53 +0900

    Update Quiz1.java

--- Quiz1.java
+++ Quiz1.java
@@ -9,11 +9,13 @@ public class Quiz1 {
 		 * 출력하는 프로그램을 작성하세요 !!
 		 */
 						
+		
 		System.out.println("이름 : 문경구");
 		System.out.println("나이 : 25");
 		System.out.println("성별 : 남");
 		System.out.println("거주지 : 부산시 연제구");
-		
+		System.out.println();
+		System.out.printf("이름 : %s\n나이 : %d\n성별 : %s\n거주지 : %s", "문경구", 25, "남", "부산시 연제구" );
 	}
 
 }
