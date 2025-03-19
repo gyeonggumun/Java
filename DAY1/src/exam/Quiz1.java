184c3e0ce52ac71d2497eaa75cbeb9711d2ba527
gyeonggumun <lion989072@gmail.com>
2025-03-19 12:42:35 +0900

    Add files via upload

---
+++ DAY1/src/exam/Quiz1.java
@@ -0,0 +1,21 @@
+package exam;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 출력문을 이용해
+		 * 자신의 이름, 나이, 성별, 거주지를
+		 * 출력하는 프로그램을 작성하세요 !!
+		 */
+						
+		
+		System.out.println("이름 : 문경구");
+		System.out.println("나이 : 25");
+		System.out.println("성별 : 남");
+		System.out.println("거주지 : 부산시 연제구");
+		System.out.println();
+		System.out.printf("이름 : %s\n나이 : %d\n성별 : %s\n거주지 : %s", "문경구", 25, "남", "부산시 연제구" );
+	}
+
+}
